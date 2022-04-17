f020f373c9fa0173f811931a15625216e83a9be7
Martin Betancur <martinbetancur27@gmail.com>
2022-04-16 20:41:06 -0500

    Mejora: Scroll con tiempo definido para permitir que el contenido se cargue completamente. Resultado: Traer alrededor de 3 veces mas referencias. Error: Algunas noticias quedaron sin raspar (es muy minima la diferencia comparado al xpath del navegador)

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ChromeOptions
 
+import time
+
 '''se puede ocultar el navegador (sin cabecera)'''
 options = ChromeOptions()
 options.headless = True
@@ -21,10 +23,10 @@ driver.get('https://finance.yahoo.com')
 #******************** SCRAPING SELENIUM *********************
 #Scrapin con Selenium
 #trajo 210 pero en la web se traen 319.
-articles = driver.find_elements(By.TAG_NAME, 'a')
+'''articles = driver.find_elements(By.TAG_NAME, 'a')
 print(len(articles))
 for elem in articles:
-    print(elem.get_attribute("href"))
+    print(elem.get_attribute("href"))'''
 
 #*********************** SCRAPING BEAUTIFULSOUP *********************
 #scraping con BS - No hay diferencia con Selenium. Trar una cantidad similar.
@@ -34,5 +36,30 @@ articles = soup.find_all('a')
 print(len(articles))
 for elem in articles:
     print(elem["href"])'''
+
+
+driver.maximize_window()
+time.sleep(1)
+#We make a slow scroll to the end of the page
+iter=1
+while True:
+        scrollHeight = driver.execute_script("return document.documentElement.scrollHeight")
+        Height=250*iter
+        driver.execute_script("window.scrollTo(0, " + str(Height) + ");")
+        if Height > scrollHeight:
+            print('End of page')
+            break
+        time.sleep(1)
+        iter+=1
+#we get the internal html code of the body        
+body = driver.execute_script("return document.body")
+source = body.get_attribute('innerHTML') 
+soup = BeautifulSoup(source, "html.parser")
+
+articles = soup.find_all('a')
+
+print(len(articles))
+for elem in articles:
+    print(elem["href"])
 
 driver.quit()
