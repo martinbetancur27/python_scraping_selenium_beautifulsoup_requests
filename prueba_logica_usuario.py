57af6f9ab93190678585fa544903ab874b129dfd
Martin Betancur <martinbetancur27@gmail.com>
2022-04-23 14:46:53 -0500

    Logica de opciones del usuario completada. Opcion por defecto y opcion personalizada

--- prueba_logica_usuario.py
+++ prueba_logica_usuario.py
@@ -1,7 +1,6 @@
 from scraper_static_page import ScraperStaticPage
 from scraper_dynamic_page import ScraperDynamicPage
 
-eleccion_por_defecto = True
 
 def static_page(page):
 
@@ -37,9 +36,6 @@ def dynamic_page(page):
         print(contents)
 
 
-def ejecutar_metodo_busqueda(metodo, busqueda):
-    pass
-
 def opciones_busqueda():
 
     #Este diccionario tendra:
@@ -53,7 +49,8 @@ def opciones_busqueda():
         6: ["Obtener por Xpath", "Ingrese la expresion Xpath (sin comillas): ", "get_by_xpath"],
         7: ["Obtener contenido de etiqueta", "Ingrese la etiqueta (sin comillas): ", "get_content_tag"]
     }
-    
+
+    print("\n**** Opciones de busqueda ****")
     #Imprimir opciones al usuario
     for key, value in opciones.items():
         print ("{:<5} {:<5}".format(key, value[0]))
@@ -76,25 +73,42 @@ def opciones_busqueda():
     #Retornar el metodo y la busqueda que selecciono el usuario
     return metodo, busqueda
 
+def prueba_personalizada():
+
+    page_url = str(input("Ingrese la URL: "))
+
+    opcion_scrape = int(input("Por favor ingrese el numero:\n1. Pagina estatica\n2. Pagina dinamica\n---> "))
+
+    if(opcion_scrape == 1):
+        print("Eleccion: pagina estatica")
+        static_page(page_url)
+
+    else:
+        print("Eleccion: pagina dinamica")
+        dynamic_page(page_url)
+
 def run():
-            
-    static_page("https://www.thoughtco.com/quotes-and-sayings-from-cervantes-3079523#:~:text=Cervantes%20Quotes%20About%20Living%20Wisely,reading%20much%20sharpens%20one's%20ingenuity.)")
+        
     
-    '''opcion = int(input("Por favor elija:\n1. Prueba por defecto\n2. Prueba personalizada"))
+    opcion = int(input("Por favor ingrese el numero:\n1. Prueba por defecto (ejemplo)\n2. Prueba personalizada\n---> "))
     
     if(opcion == 1):
-        while(True):
 
-            opcion_defecto = int(input("Por favor elija:\n1. Prueba pagina estatica\n2. Prueba pagina dinamica\n3. Salir"))
+        opcion_defecto = int(input("Por favor ingrese el numero:\n1. Prueba pagina estatica\n2. Prueba pagina dinamica\n---> "))
+        
+        if opcion_defecto == 1:
+            print("Eleccion: pagina estatica")
+            static_page("https://www.thoughtco.com/quotes-and-sayings-from-cervantes-3079523#:~:text=Cervantes%20Quotes%20About%20Living%20Wisely,reading%20much%20sharpens%20one's%20ingenuity.)")
             
-            if opcion_defecto == 1:
-                url_page = "https://www.thoughtco.com/quotes-and-sayings-from-cervantes-3079523#:~:text=Cervantes%20Quotes%20About%20Living%20Wisely,reading%20much%20sharpens%20one's%20ingenuity.)"
-                
-                static_page()
-            elif opcion_defecto == 2:
-                dynamic_page("https://finance.yahoo.com")
-            else:
-                break'''
+        else:
+            print("Eleccion: pagina dinamica")
+            dynamic_page("https://finance.yahoo.com")
+            
+    else:
+
+        prueba_personalizada()
+        
+        
 
 if __name__ == "__main__":
 
